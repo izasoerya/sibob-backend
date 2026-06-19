3d0eb7eacca340f61a6eac129f110a820a806af2
izasoerya <ihza.surya@gmail.com>
2026-06-19 08:14:50 +0700

    chore: separate env for each device

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ElegantOTA.h>
 #include "models.h"
 #include "connection_wifi.h"
+#include "esp_task_wdt.h"
 
 #include "sensor_reading/dht_reading.h"
 #include "sensor_reading/ds18_reading.h"
@@ -10,9 +11,6 @@
 #include "sensor_reading/ph_reading.h"
 #include "sensor_reading/soil_hum_reading.h"
 #include "control_system/bang-bang.h"
-
-// #define MAIN_PROGRAM
-#define CALIBRATE_HX711_PROGRAM
 
 #define PIN_FAN 0
 #define PIN_MIST 1
@@ -23,9 +21,11 @@
 #define PIN_EN_PH 8
 #define PIN_CLK 10
 
-#ifndef MAIN_PROGRAM
-
-WiFiConnection wifi("Subhanallah", "muhammadnabiyullah", "sibob-1");
+#if defined(SIBOB_1)
+WiFiConnection wifi("Bengkel Inovasi Indonesia", "EKSPEKTASI", "sibob-1");
+#elif defined(SIBOB_2)
+WiFiConnection wifi("Bengkel Inovasi Indonesia", "EKSPEKTASI", "sibob-2");
+#endif
 WebServer server(80);
 SensorData sensors;
 
@@ -33,17 +33,24 @@ DHTWrapper dht(PIN_DHT);
 PHWrapper ph(PIN_EN_PH);
 HX711 hx1;
 HX711 hx2;
-OneWire oneWire(2); // TODO: CHANGE PIN
+OneWire oneWire(9);
 DS18B20 ds(&oneWire);
 ADS1115 ads(0x48);
 ADSWrapper adsWrapper(ads);
 
 void setup()
 {
+	esp_task_wdt_init(30, true);
+	esp_task_wdt_add(NULL);
+
 	Serial.begin(115200);
 	wifi.begin();
 	ElegantOTA.begin(&server);
 	ElegantOTA.setAutoReboot(true);
+
+	pinMode(0, OUTPUT);
+	pinMode(1, OUTPUT);
+	pinMode(3, OUTPUT);
 
 	server.on("/", []()
 			  { 
@@ -70,112 +77,40 @@ void setup()
 	sensors.humidity_soil.status = sensors.ph.status;
 }
 
+uint64_t lastUpdate = 0;
+
 void loop()
 {
 	wifi.reconnect();
+	esp_task_wdt_reset();
 
-	sensors.temperature_air.value = dht.getTemperature();
-	sensors.humidity_air.value = dht.getHumidity();
-	sensors.weight_breed.value = weightReading(hx1);
-	sensors.weight_yield.value = weightReading(hx2);
-	sensors.temperature_soil.value = soilTemperatureReading(ds);
-	sensors.humidity_soil.value = soilHumidityReading(adsWrapper);
-	sensors.ph.value = ph.phReading(adsWrapper);
+	if (millis() > lastUpdate + 10000)
+	{
+#if defined(SIBOB_1)
+		sensors.id = 1;
+		sensors.weight_breed.value = (478804.59 - weightReading(hx1)) * 0.0075775;
+		sensors.weight_yield.value = (-232862.92 - weightReading(hx2)) * 0.007118;
+#else defined(SIBOB_2)
+		sensors.id = 2;
+		sensors.weight_breed.value = (478804.59 - weightReading(hx1)) * 0.0075775;
+		sensors.weight_yield.value = (-232862.92 - weightReading(hx2)) * 0.007118;
+#endif
+		sensors.temperature_air.value = dht.getTemperature();
+		sensors.humidity_air.value = dht.getHumidity();
+		sensors.temperature_soil.value = soilTemperatureReading(ds);
+		sensors.humidity_soil.value = soilHumidityReading(adsWrapper);
+		sensors.ph.value = ph.phReading(adsWrapper);
 
-	static const BangBangController bang(
-		BangBangConfig{40, 20, 90, 40}, // top temp, bot temp, top hum, bot hum
-		ActuatorConfig{0, 1, 3});		// pin fan, pin mist, pin heater
-	bang.control(sensors.temperature_soil.value, sensors.humidity_soil.value);
+		static const BangBangController bang(
+			BangBangConfig{25, 10, 90, 40}, // top temp, bot temp, top hum, bot hum
+			ActuatorConfig{0, 1, 3});		// pin fan, pin mist, pin heater
+		bang.control(sensors.temperature_soil.value, sensors.humidity_soil.value);
 
-	wifi.postSupabase(sensors.toJsonDocument());
+		wifi.postSupabase(sensors.toJsonDocument());
+		serializeJsonPretty(sensors.toJsonDocument(), Serial);
+		lastUpdate = millis();
+	}
 
 	ElegantOTA.loop();
 	server.handleClient();
 }
-
-#endif
-
-#ifndef CALIBRATE_HX711_PROGRAM
-
-HX711 myScale;
-
-//  adjust pins if needed.
-uint8_t dataPin = 4;
-uint8_t clockPin = 10;
-
-void setup()
-{
-	Serial.begin(115200);
-	Serial.println();
-	Serial.println(__FILE__);
-	Serial.print("HX711_LIB_VERSION: ");
-	Serial.println(HX711_LIB_VERSION);
-	Serial.println();
-
-	myScale.begin(dataPin, clockPin);
-}
-
-void loop()
-{
-	calibrate();
-}
-
-void calibrate()
-{
-	Serial.println("\n\nCALIBRATION\n===========");
-	Serial.println("remove all weight from the loadcell");
-	//  flush Serial input
-	while (Serial.available())
-		Serial.read();
-
-	Serial.println("and press enter\n");
-	while (Serial.available() == 0)
-		;
-
-	Serial.println("Determine zero weight offset");
-	//  average 20 measurements.
-	myScale.tare(20);
-	int32_t offset = myScale.get_offset();
-
-	Serial.print("OFFSET: ");
-	Serial.println(offset);
-	Serial.println();
-
-	Serial.println("place a weight on the loadcell");
-	//  flush Serial input
-	while (Serial.available())
-		Serial.read();
-
-	Serial.println("enter the weight in (whole) grams and press enter");
-	uint32_t weight = 0;
-	while (Serial.peek() != '\n')
-	{
-		if (Serial.available())
-		{
-			char ch = Serial.read();
-			if (isdigit(ch))
-			{
-				weight *= 10;
-				weight = weight + (ch - '0');
-			}
-		}
-	}
-	Serial.print("WEIGHT: ");
-	Serial.println(weight);
-	myScale.calibrate_scale(weight, 20);
-	float scale = myScale.get_scale();
-
-	Serial.print("SCALE:  ");
-	Serial.println(scale, 6);
-
-	Serial.print("\nuse scale.set_offset(");
-	Serial.print(offset);
-	Serial.print("); and scale.set_scale(");
-	Serial.print(scale, 6);
-	Serial.print(");\n");
-	Serial.println("in the setup of your project");
-
-	Serial.println("\n\n");
-}
-
-#endif
